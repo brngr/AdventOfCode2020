9c2b6178651e8f6d0b0a17fb8866df47eed349cb
Berenger <bere@nbk-10.leaf.space>
2020-12-01 13:39:32 +0100

    Day 1 - 2 Stars

--- Day1/solution.py
+++ Day1/solution.py
@@ -5,12 +5,12 @@ def main():
     f = open(sys.argv[1],"r")
     puzzle_input = list(map(int,f.readlines()))
     f.close()
-
     for i in puzzle_input:
         for j in puzzle_input:
-            if i + j == 2020:
-                print(i*j)
-                return
+            for k in puzzle_input:
+                if i + j + k == 2020:
+                    print(i * j * k)
+                    return
 
 if __name__ == "__main__":
     main()
