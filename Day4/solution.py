93707ea263d72d9dd248b14bac1070461e96e3b7
Berenger <bere@nbk-10.leaf.space>
2020-12-04 14:01:51 +0100

    Working on it 🤹‍♀️

--- Day4/solution.py
+++ Day4/solution.py
@@ -7,11 +7,51 @@ class passport:
     def __init__(self, data):
         self.data = data
         self.isValid = self.checkValiditiy()
+        self.byr = None
+        self.iyr = None
+        self.eyr = None
+        self.hgt = None
+        self.hcl = None
+        self.ecl = None
+        self.pid = None
+        self.cid = None
+        if self.isValid:
+            self.getData()
+            self.showData()
+        return
+
+    def getFieldData(self, field, dataLength):
+        fieldPos = self.data.find(field)
+        if fieldPos != -1:
+            value = self.data[
+                fieldPos + len(field) + 1 : fieldPos + len(field) + 1 + dataLength
+            ]
+        else:
+            return 0
+        return
+
+    def showData(self):
+        print("byr: " + str(self.byr))
+        print("iyr: " + str(self.iyr))
+        print("eyr: " + str(self.eyr))
+        print("hgt: " + str(self.hgt))
+        print("hcl: " + str(self.hcl))
+        print("ecl: " + str(self.ecl))
+        print("pid: " + str(self.pid))
+
+    def getData(self):
+        self.byr = self.getFieldData("byr", 4)
+        self.iyr = self.getFieldData("iyr", 4)
+        self.eyr = self.getFieldData("eyr", 4)
+        # self.hgt = self.getFieldData("hgt", 0)
+        self.hcl = self.getFieldData("hcl", 7)
+        self.ecl = self.getFieldData("ecl", 3)
+        self.pid = self.getFieldData("pid", 9)
         return
 
     def checkValiditiy(self):
         # rewrite with regex
-        if "byr:" not in self.data:
+        if "byr:" in self.data:
             return False
         if "iyr:" not in self.data:
             return False
@@ -25,8 +65,6 @@ class passport:
             return False
         if "pid:" not in self.data:
             return False
-        # if "cid" not in self.data:
-        #    return False
         return True
 
 
@@ -53,9 +91,10 @@ def main():
     for p in passportData:
         passportBuffer = passport(p)
         # print(passportBuffer.data)
-        if passportBuffer.isValid == True:
-            count += 1
-    print(count)
+        # if passportBuffer.isValid == True:
+        #    count += 1
+    # print(count)
+    print("---------------")
     return
 
 
