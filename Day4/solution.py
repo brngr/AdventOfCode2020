3e71a8da9954b3e27e079ab7d74c2bcda80c9ae5
brngr <berenger.villat@gmail.com>
2020-12-05 18:56:16 +0100

    Day 4 - ⭐⭐ Regex ❤

--- Day4/solution.py
+++ Day4/solution.py
@@ -3,14 +3,16 @@ import sys
 import cProfile
 import re
 
-RE_BYR = "byr:"
-RE_IYR = "iyr:"
-RE_EYR = "eyr:"
-RE_HGT = "hgt:"
-RE_HCL = "hcl:"
-RE_ECL = "ecl:"
-RE_PID = "pid:"
-RE_CID = "cid:"
+
+RE_BYR = "byr:\d{4} "
+RE_IYR = "iyr:\d{4} "
+RE_EYR = "eyr:\d{4} "
+RE_HGT = "hgt:\d{2,}(cm|in) "
+RE_HCL = "hcl:#[a-f0-9]{6}"
+RE_ECL = "ecl:[a-z]{3} "
+RE_PID = "pid:[0-9]{9} "
+# RE_CID = "cid:"
+
 re_byr = re.compile(RE_BYR)
 re_iyr = re.compile(RE_IYR)
 re_eyr = re.compile(RE_EYR)
@@ -18,34 +20,13 @@ re_hcl = re.compile(RE_HGT)
 re_hgt = re.compile(RE_HCL)
 re_ecl = re.compile(RE_ECL)
 re_pid = re.compile(RE_PID)
-re_cid = re.compile(RE_CID)
-
-# Part 2
-# RE_BYR = "byr:\d{4}"
-# RE_IYR = "iyr:\d{4}"
-# RE_EYR = "eyr:\d{4}"
-# RE_HGT = "hgt:"
-# RE_HCL = "hcl:#[a-f0-9]{6}"
-# RE_ECL = "ecl:[a-z]{3}"
-# RE_PID = "pid:[0-9]{9}"
-# RE_CID = "cid:"
-
-# byr (Birth Year) - four digits; at least 1920 and at most 2002.
-# iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-# eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-# hgt (Height) - a number followed by either cm or in:
-#    If cm, the number must be at least 150 and at most 193.
-#    If in, the number must be at least 59 and at most 76.
-# hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-# ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-# pid (Passport ID) - a nine-digit number, including leading zeroes.
-# cid (Country ID) - ignored, missing or not.
+# re_cid = re.compile(RE_CID)
 
 
 class passport:
     def __init__(self, data):
         self.data = data
-        self.isValid = self.checkValiditiy()
+        self.isValid = False
         self.byr = None
         self.iyr = None
         self.eyr = None
@@ -54,10 +35,8 @@ class passport:
         self.ecl = None
         self.pid = None
         self.cid = None
-        # if self.isValid:
-        #    self.getData()
-        #    # self.showData()
-        #    self.isValid = self.checkValues()
+        if self.getData() == True:
+            self.isValid = self.checkValues()
         return
 
     def showData(self):
@@ -70,31 +49,69 @@ class passport:
         print("pid: " + str(self.pid))
         print("")
 
-    def checkValiditiy(self):
-        if re_byr.search(self.data) is None:
-            return False
-        if re_iyr.search(self.data) is None:
-            return False
-        if re_eyr.search(self.data) is None:
-            return False
-        if re_hcl.search(self.data) is None:
-            return False
-        if re_hgt.search(self.data) is None:
-            return False
-        if re_ecl.search(self.data) is None:
-            return False
-        if re_pid.search(self.data) is None:
-            return False
+    def getData(self):
+        tmp = re_byr.search(self.data)
+        if tmp != None:
+            self.byr = int(
+                self.data[tmp.span()[0] : tmp.span()[1]].split(":")[1].strip()
+            )
+        tmp = re_iyr.search(self.data)
+        if tmp != None:
+            self.iyr = int(
+                self.data[tmp.span()[0] : tmp.span()[1]].split(":")[1].strip()
+            )
+        tmp = re_eyr.search(self.data)
+        if tmp != None:
+            self.eyr = int(
+                self.data[tmp.span()[0] : tmp.span()[1]].split(":")[1].strip()
+            )
+        tmp = re_hcl.search(self.data)
+        if tmp != None:
+            self.hgt = self.data[tmp.span()[0] : tmp.span()[1]].split(":")[1].strip()
+        tmp = re_hgt.search(self.data)
+        if tmp != None:
+            self.hcl = self.data[tmp.span()[0] : tmp.span()[1]].split(":")[1].strip()
+        tmp = re_ecl.search(self.data)
+        if tmp != None:
+            self.ecl = self.data[tmp.span()[0] : tmp.span()[1]].split(":")[1].strip()
+        tmp = re_pid.search(self.data)
+        if tmp != None:
+            self.pid = self.data[tmp.span()[0] : tmp.span()[1]].split(":")[1]
         return True
 
-    def getData(self):
-        return
-
-    def getFieldData(self, field, dataLength):
-        return
-
     def checkValues(self):
-        return
+        if self.byr is None or self.byr not in range(1920, 2003):
+            return False
+        if self.iyr is None or self.iyr not in range(2010, 2021):
+            return False
+        if self.eyr is None or self.eyr not in range(2020, 2031):
+            return False
+        if self.hgt is not None:
+            value = int(self.hgt[0 : len(self.hgt) - 2])
+            unit = self.hgt[len(self.hgt) - 2 : len(self.hgt)]
+            if unit == "cm":
+                if value not in range(150, 194):
+                    return False
+            elif unit == "in":
+                if value not in range(59, 77):
+                    return False
+        else:
+            return False
+        if self.hcl is None:
+            return False
+        if self.ecl is None or self.ecl not in [
+            "amb",
+            "blu",
+            "brn",
+            "gry",
+            "grn",
+            "hzl",
+            "oth",
+        ]:
+            return False
+        if self.pid is None or self.pid is None:
+            return False
+        return True
 
 
 def main():
@@ -103,20 +120,20 @@ def main():
     batchFile = list(map(str, f.readlines()))
     f.close()
     passportData = []
-    # print(batchFile)
+
     for i in range(len(batchFile)):
         if batchFile[i] == "\n":
-            passportData.append(stringBuffer.strip())
+            passportData.append(stringBuffer.strip() + " ")
             stringBuffer = ""
         elif i == len(batchFile) - 1:
             stringBuffer += batchFile[i].replace("\n", " ")
-            passportData.append(stringBuffer.strip())
+            passportData.append(stringBuffer.strip() + " ")
             stringBuffer = ""
         else:
             stringBuffer += batchFile[i].replace("\n", " ")
 
     count = 0
-    # print(passportData)
+
     for p in passportData:
         passportBuffer = passport(p)
         if passportBuffer.isValid == True:
@@ -127,5 +144,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
-    # cProfile.run("main()")
+    # main()
+    cProfile.run("main()")
