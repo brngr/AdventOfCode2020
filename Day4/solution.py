8a955a0fd9dd4b12a9b1b438e52d9668209ebc8e
brngr <berenger.villat@gmail.com>
2020-12-05 16:29:37 +0100

    Day 4 - Before rewriting...

--- Day4/solution.py
+++ Day4/solution.py
@@ -18,7 +18,8 @@ class passport:
         self.cid = None
         if self.isValid:
             self.getData()
-            self.showData()
+            # self.showData()
+            self.isValid = self.checkValues()
         return
 
     def getFieldData(self, field, dataLength):
@@ -29,21 +30,97 @@ class passport:
                     fieldPos + len(field) + 1 : fieldPos + len(field) + 1 + dataLength
                 ]
                 return value
-
             else:
-                print("dgdfs")
                 flag = True
                 buffer = ""
                 i = 0
                 val = ""
                 while flag:
-                    val = self.data[fieldPos + len(field) + 1 + i]
-                    buffer += val
+                    # print(self.data)
+                    # print(fieldPos)
+                    # print(buffer)
+                    buffer += self.data[fieldPos + len(field) + 1 + i]
+                    i += 1
                     if "cm" in buffer or "in" in buffer:
                         flag = False
+                    if (
+                        len(buffer) > 5
+                        or fieldPos + len(field) + 1 + i > len(self.data) - 1
+                    ):
+                        return "00cm"
                 return buffer
-
         return
+
+    def checkValues(self):
+        print(self.data)
+        print("byr")
+        # byr (Birth Year) - four digits; at least 1920 and at most 2002.
+        if self.byr < 1920 or self.byr > 2002:
+            return False
+        print("iyr")
+        # iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+        if self.iyr < 2010 or self.iyr > 2020:
+            return False
+        print("eyr")
+        # eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+        if self.eyr < 2020 or self.eyr > 2030:
+            return False
+        print("hgt")
+        # hgt (Height) - a number followed by either cm or in:
+        #
+        #    If cm, the number must be at least 150 and at most 193.
+        #    If in, the number must be at least 59 and at most 76.
+        unit = self.hgt[len(self.hgt) - 2] + self.hgt[len(self.hgt) - 1]
+        height = int(self.hgt[0 : len(self.hgt) - 2])
+        print(height)
+        if unit == "cm":
+            if height < 150 or height > 193:
+                return False
+        elif unit == "in":
+            if height < 59 or height > 76:
+                return False
+        print("hcl")
+        # hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+        if len(self.hcl) == 7:
+            if self.hcl[0] == "#":
+                for l in self.hcl[1:7]:
+                    if l not in [
+                        "0",
+                        "1",
+                        "2",
+                        "3",
+                        "4",
+                        "5",
+                        "6",
+                        "7",
+                        "8",
+                        "9",
+                        "a",
+                        "b",
+                        "c",
+                        "d",
+                        "e",
+                        "f",
+                    ]:
+                        return False
+            else:
+                return False
+        else:
+            return False
+        print("ecl")
+        # ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+        if self.ecl not in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]:
+            return False
+        print("pid")
+        # pid (Passport ID) - a nine-digit number, including leading zeroes.
+        if len(self.pid) == 9:
+            for a in self.pid:
+                if int(a) not in range(0, 10):
+                    return False
+        else:
+            return False
+
+        return True
 
     def showData(self):
         print("byr: " + str(self.byr))
@@ -53,11 +130,12 @@ class passport:
         print("hcl: " + str(self.hcl))
         print("ecl: " + str(self.ecl))
         print("pid: " + str(self.pid))
+        print("")
 
     def getData(self):
-        self.byr = self.getFieldData("byr", 4)
-        self.iyr = self.getFieldData("iyr", 4)
-        self.eyr = self.getFieldData("eyr", 4)
+        self.byr = int(self.getFieldData("byr", 4))
+        self.iyr = int(self.getFieldData("iyr", 4))
+        self.eyr = int(self.getFieldData("eyr", 4))
         self.hgt = self.getFieldData("hgt", 0)
         self.hcl = self.getFieldData("hcl", 7)
         self.ecl = self.getFieldData("ecl", 3)
@@ -105,9 +183,9 @@ def main():
     # print(passportData)
     for p in passportData:
         passportBuffer = passport(p)
-        # if passportBuffer.isValid == True:
-        #    count += 1
-        # print(count)
+        if passportBuffer.isValid == True:
+            count += 1
+    print(count)
 
     return
 
