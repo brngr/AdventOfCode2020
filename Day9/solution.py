30a624ddb94ec90e14d815c7fa7b84bc9efa535d
brngr <berenger.villat@gmail.com>
2020-12-09 22:37:52 +0100

    Day 9 - ⭐⭐

--- Day9/solution.py
+++ Day9/solution.py
@@ -19,19 +19,43 @@ def findSum(goal, values):
     return goal
 
 
+def sumList(values):
+    buffer = 0
+    for h in range(0, len(values)):
+        buffer += values[h]
+    return buffer
+
+
+def findSummingRange(invalid, values):
+    a = 0
+    for i in range(0, len(values)):
+        for j in range(i, len(values)):
+            a = sumList(values[i : j + 1])
+            if a == invalid:
+                return values[i : j + 1]
+            if a > invalid:
+                continue
+    return None
+
+
 def main():
     f = open(sys.argv[1], "r")
+
     series = list(map(int, f.readlines()))
     f.close()
 
     input_length = len(series)
-
+    weakness = 0
     goal = 0
     for i in range(PREAMBLE_LENGTH, input_length):
         goal = series[i]
-        if findSum(goal, series[i - PREAMBLE_LENGTH : i]) != 0:
-            print(goal)
+        invalid = findSum(goal, series[i - PREAMBLE_LENGTH : i])
+        if invalid != 0:
             break
+
+    summingRange = findSummingRange(invalid, series)
+    weakness = min(summingRange) + max(summingRange)
+    print(weakness)
     return
 
 
