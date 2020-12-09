a32970f35ad7318990641fea6d37605d0e69afd6
brngr <berenger.villat@gmail.com>
2020-12-09 21:16:43 +0100

    Day 9 - ⭐

--- Day9/solution.py
+++ Day9/solution.py
@@ -2,12 +2,21 @@
 import sys
 import cProfile
 
-PREAMBLE_LENGTH = 5
+PREAMBLE_LENGTH = 25
 
 
 def findSum(goal, values):
-    print(goal, values)
-    return
+    a = 0
+    b = 0
+    c = 0
+    for i in range(0, len(values)):
+        a = values[i]
+        for j in range(i + 1, len(values)):
+            b = values[j]
+            c = a + b
+            if c == goal:
+                return 0
+    return goal
 
 
 def main():
@@ -18,11 +27,14 @@ def main():
     input_length = len(series)
 
     goal = 0
-    for i in range(5, input_length):
+    for i in range(PREAMBLE_LENGTH, input_length):
         goal = series[i]
-        findSum(goal, series[i - PREAMBLE_LENGTH : i])
+        if findSum(goal, series[i - PREAMBLE_LENGTH : i]) != 0:
+            print(goal)
+            break
+    return
 
 
 if __name__ == "__main__":
-    main()
-    # cProfile.run("main()")
+    # main()
+    cProfile.run("main()")
